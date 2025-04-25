25b580fdc714833b1fb9d0f9a7fb0ed116c833ce
kanalabhargavi0108 <kanalabhargavi18@gmail.com>
2025-04-25 20:24:08 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "commonstatefile"
-    key    = "ec2/terraform.tfstate"
+    bucket = "bhargavi123456"
+    key    = "k8/terraform.tfstate"
     region = "us-east-1"
   }
 
